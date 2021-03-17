2d4ab7a97f066c87cad40b5f4c502473300f5a62
Jan Vinkenroye <janvinkenroye@gmail.com>
2021-03-17 09:39:20 -0700

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+    <Button text="Add task" color="green"></Button>
+    <Button text="Update task" color="blue"></Button>
+    <Button text="Delete task" color="red"></Button>
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+export default {
+  name: "Header",
+  components: {
+    Button
+  },
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+  header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
